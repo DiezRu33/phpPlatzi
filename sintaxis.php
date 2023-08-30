ff050017be960bd595a97fbe6326df8cb2ab1d76
diego <diegoruffa@hotmail.com>
2023-08-30 05:36:20 -0300

    fundamentos

--- sintaxis.php
+++ sintaxis.php
@@ -50,14 +50,34 @@ $nombreDePepe = "Pepito Grillo";
 echo $nombreDeMario ?? $nombreDePepe;
 */
 
-/* Precedencia */
+/* Precedencia 
+    $contador = 1;
+    $resultado = $contador++;
 
-$contador = 1;
-$resultado = $contador++;
+    echo $contador . "\n";
+    echo $resultado;
+*/
 
-echo $contador . "\n";
-echo $resultado;
+/* --- TIEMPO usando precedencia ---
 
+    $horas = (int) ($segundos / 3600);
+    $segundos = (int) ($segundos % 3600);
+    $minutos =  (int) ($segundos / 60);
+    $segundos = (int) ($segundos % 60);
+
+    echo "Son $horas\hs, $minutos minutos y $segundos segundos";
+*/
+
+/* Reto de clase -----------------------------
+
+    $horas = readline("Ingresa la hora: ");
+    $minutos = readline("Ingresa la cantidad de tiempo en minutos: ");
+    $segundos = readline("Ingresa el tiempo en segundos: ");
+
+    $resultadoFinal = ($horas*3600) + ($minutos*60) + ($segundos);
+
+echo "El resultado es: $resultadoFinal";
+*/
 
 echo "\n";
 
